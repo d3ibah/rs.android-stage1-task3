75162e070adaf192f24cf53fb0f6a5c8a29bdaf8
alex.dzervanouski <alex.dzervanouski@exposit.com>
2020-05-06 01:59:22 +0300

    Task 2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,23 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val c = array[0]
+        val n = array[1]
+        var bruteforceC: Long = 0
+        var k: Int = 0
+
+        for (i in 1..n) {
+            k = i
+            bruteforceC = factorial(n) / (factorial(n - k) * factorial(k))
+
+            if (bruteforceC == c.toLong()) return k
+
+            if (bruteforceC > c) return k
+        }
+
+        if (bruteforceC > c) return k else return null
     }
+
+    fun factorial(n: Int): Long = if (n < 2) 1 else n * factorial(n - 1)
 }
