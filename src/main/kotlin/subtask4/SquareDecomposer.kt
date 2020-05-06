feb9059ca19e84222ba690a6ff832505f4bae6fd
alex.dzervanouski <alex.dzervanouski@exposit.com>
2020-05-06 16:58:18 +0300

    Task 4 Draft

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -6,60 +6,32 @@ import kotlin.math.sqrt
 class SquareDecomposer {
 
     fun decomposeNumber(number: Int): Array<Int>? {
+        if (number < 3) return null
+        val square = number * number
+        val previousRootSquare = number - 1
 
-        var square = number * number
-//
-//        var nextNumber = number - 1
-//
-//        var nextSquare = nextNumber * nextNumber
-//
-//        var remain = square - nextSquare
-//
-//        if (remain > 1) {
-//            var rootSquare = sqrt(remain.toDouble()).toInt()
-//        }
-
-//        val squareList = mutableListOf<Int>()
-//        val someObjectList = mutableListOf<SomeObject>()
-//        for (num in number-1 downTo 1 step 1) {
-//            squareList.add(num*num)
-//        }
-
-
-//        getNextPart(number - 1)?.let {
-//            someObjectList.add(it)
-//        }
-
-//        squareList.forEach {
-//
-//            println("square is $it")
-//        }
-
-//        throw NotImplementedError("Not implemented")
-        return getNextPart(square, number)
+        val temp = getMemberList(square, previousRootSquare)
+        return temp
     }
 
-    fun getNextPart(inputRemain: Int, previousRootSquare: Int): Array<Int>? {
-        for (currentRoot in previousRootSquare downTo 1) {
-            var currentRemain = inputRemain - currentRoot.toDouble().pow(2)
+    private fun getMemberList(inputRemain: Int, previousRootSquare: Int): Array<Int>? {
+        for (currentRoot in previousRootSquare downTo 1 step 1) {
+            val currentRemain = inputRemain - currentRoot.toDouble().pow(2)
+
             if (currentRemain < 0) return null
-            if (currentRemain == 0.0) return arrayOf(currentRoot.toInt())
+
+            if (currentRemain == 0.0) return arrayOf(currentRoot)
 
             val square = sqrt(currentRemain).toInt()
             val nextSquareRoot =
-                if (square >= currentRoot) getNextPart(currentRemain.toInt(), currentRoot - 1)
-                else getNextPart(currentRemain.toInt(), square)
+                if (square >= currentRoot) getMemberList(currentRemain.toInt(), currentRoot - 1)
+                else getMemberList(currentRemain.toInt(), square)
 
-            return if (nextSquareRoot?.isNotEmpty() == true) { nextSquareRoot + arrayOf(currentRoot) }
-            else null
+            return if (nextSquareRoot?.isNotEmpty() == true) {
+                nextSquareRoot + arrayOf(currentRoot)
+            } else null
         }
 
         return null
     }
-}
-
-class SomeObject(private val inputRemain: Int) {
-    var rootSquare = sqrt(inputRemain.toDouble()).toInt()
-    var outputRemain = inputRemain - (rootSquare * rootSquare)
-        get
 }
